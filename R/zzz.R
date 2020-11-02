2ac1c8caf8fdc1e5d6b00763a7d9078ad50c4a0f
kovidhv <kovidh.v14@gmail.com>
2020-11-02 10:21:42 -0500

    package loading message

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 
 #########################################################################################################
-# Load R data objects when SBGNview package is loaded
+# Load R data objects when SBGNview package is loaded and print startup message
 .onLoad <- function(libname, pkgname){
   pkgNames = rownames(installed.packages())
   
@@ -8,6 +8,11 @@
     data("pathways.info", package = "SBGNview")
     data("mapped.ids", package = "SBGNview")
   }
+  
+  message = "\n##############################################################################\nSBGNview is an open source R software package distributed under GNU General Public License version 3 (GPLv3). Details of GPLv3 are available at http://www.gnu.org/licenses/gpl-3.0.html. For more information about SBGNview, visit https://bioconductor.org/packages/release/bioc/html/SBGNview.html.\n\nTo get started, run browseVignettes(\"SBGNview\") to view vignettes for SBGNview. Use ls(\"package:SBGNview\") to get list of avaliable functions and use help(\"<functionName>\") to access a function's documentation.\n##############################################################################\n"
+  
+  packageStartupMessage(message)
+  
 }
 
 #########################################################################################################
